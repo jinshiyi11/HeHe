c8b7179688a0af06ee54efad75bc43c6fc0cc03e
shuaiweican <49662990@qq.com>
2014-03-31 00:55:16 +0800

    1.init hehe client

---
+++ src/com/shuai/hehe/MainActivity.java
@@ -0,0 +1,28 @@
+package com.shuai.hehe;
+
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.Window;
+
+public class MainActivity extends Activity {
+	
+	private PullToRefreshListView mListView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
